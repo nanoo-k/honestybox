2479b2d7cefbdf1ad8226d7ff5626f960f3b7d45
Miguel Valencia <miguel.s.valencia@gmail.com>
2013-12-27 14:24:07 -0800

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Honestybox::Application.config.secret_token = 'efd521fdacc2a2fa88e0ef77e797d54c42f72d48a0f3b89b75b134e273be547e2d0407c37a2212d3f974b9c968193164a4b0b23f55f4aea480db4484717eac71'
